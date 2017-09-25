5eb1a944d53a9a910c2a549d7d8cef219aa0597e
sergey_utkin <sergey_utkin@sonm.io>
2017-09-25 15:48:52 +0300

    WIP antd theme

--- workspaces/front/src/components/layouts/login/index.tsx
+++ workspaces/front/src/components/layouts/login/index.tsx
@@ -2,22 +2,37 @@ import * as React from 'react';
 import * as cn from 'classnames';
 import { Form, Button, Input, Icon } from 'antd';
 import { FormComponentProps } from 'antd/lib/form/Form';
-import { api } from 'api';
+import { api, IPrivateKey } from 'api';
+import { inject } from 'mobx-react';
 
 export interface IProps extends FormComponentProps {
   className?: string;
+  privateKey?: IPrivateKey;
 }
 
 export interface IState extends FormComponentProps {
-
+  privateKey?: IPrivateKey;
 }
 
-class LoginInner extends React.Component<IProps, IState> {
+@inject('privateKey')
+class LoginInner extends React.Component<IProps, object> {
+  public state = {
+    privateKey: this.props.privateKey,
+  };
+
   public render() {
     const {
       className,
       form,
     } = this.props;
+
+    const {
+      privateKey,
+    } = this.props;
+
+    if (privateKey !== null) {
+      return this.renderFileOpen();
+    }
 
     return (
       <div className="sonm-login">
@@ -41,6 +56,14 @@ class LoginInner extends React.Component<IProps, IState> {
             </Button>
           </Form>
         </div>
+      </div>
+    );
+  }
+
+  private renderFileOpen() {
+    return (
+      <div className="sonm-open-key">
+
       </div>
     );
   }
