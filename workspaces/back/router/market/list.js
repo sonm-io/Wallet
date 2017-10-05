632f145a896a86768d6b6c38721f0328f7831339
dmitrypisanko <dmitry.pisanko@gmail.com>
2017-10-05 19:05:05 +0300

    Start GRPC integration

--- workspaces/back/router/market/list.js
+++ workspaces/back/router/market/list.js
@@ -1,7 +1,13 @@
 'use strict';
 
-//const grpc = require('grpc');
+const grpc = require('grpc');
+
+const sonmGRPC = grpc.load(__dirname + '/../../proto/marketplace.proto').sonm;
+const market = new sonmGRPC.Market('localhost:50051', grpc.credentials.createInsecure());
 
 module.exports = async function(app) {
+    const res = await market.GetOrderByID({id: '123'});
+    console.log(res);
+
     console.log('111');
 };
