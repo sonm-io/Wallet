004a05ba1b4d99bbf79697d3855daccdf29618d7
sergey_utkin <sergey_utkin@sonm.io>
2017-12-04 16:31:19 +0300

    remove mock api

--- front/src/app/api/index.ts
+++ front/src/app/api/index.ts
@@ -1,95 +1,119 @@
 // import { createPromise } from './ipc';
+
+import { messages } from './error-messages';
+import * as ipc from './ipc';
 import {
     ISendTransactionResult,
     IAccountInfo,
     ICurrencyInfo,
     ISendTransaction,
     IResult,
+    IResponse,
+    IValidation,
+    ITxListFilter,
 } from './types';
-// FIXME remove
-import * as mock from './mock';
+
 export * from './types';
 
-export class Api {
-    public static async setSecretKey(password: string, name: string): Promise<IResult<boolean>>  {
-        let validation;
-        let data = true;
+const MAX_DELAY_DEFAULT = 10000;
 
-        if (password !== 'my secret key') {
-            validation = {
-                password: 'invalid_master_password',
-            };
-            data = false;
-        }
+let count = 0;
+function nextRequestId(): string {
+    return 'request' + count++;
+}
 
-        await mock.delay(10);
+function createPromise(
+    type: string,
+    payload?: any,
+    maxDelay: number = MAX_DELAY_DEFAULT,
+): Promise<IResult<any>> {
+    return new Promise((done, reject) => {
+        const requestId = nextRequestId();
 
-        return {
-            data,
-            validation,
+        const callback = (event: any, response: IResponse<any>) => {
+            if (response.validation) {
+                response.validation = processValidation(response.validation);
+            }
+
+            if (response.success) {
+                // TODO remove
+                console.log(type, response);
+                //
+                done(response);
+            } else {
+                reject(response.error);
+            }
         };
+
+        ipc.once(requestId, callback);
+
+        (ipc as any).send({
+            requestId,
+            type,
+            payload,
+        });
+    });
+}
+
+function processValidation(obj: any): IValidation {
+    return Object.keys(obj).reduce((acc: IValidation, key: string) => {
+        acc[key] = messages[obj[key]];
+        return acc;
+    }, {});
+}
+
+export class Api {
+    public static async setSecretKey(password: string, walletName: string): Promise<IResult<boolean>>  {
+        return createPromise('account.setSecretKey', { password, walletName });
+    }
+
+    public static async getWalletList(): Promise<IResult<string[]>>  {
+        return createPromise('getWalletList');
+    }
+
+    public static async hasSavedData(): Promise<IResult<boolean>>  {
+        return createPromise('account.hasSavedData');
     }
 
     public static async addAccount(jsonRaw: string, password: string, name: string): Promise<IResult<IAccountInfo>> {
-
-        await mock.delay(10);
-
-        return {
-            data: mock.newAccount,
-        };
+        return createPromise('account.add', { json: jsonRaw, password, name });
     }
 
     public static async removeAccount(address: string): Promise<IResult<boolean>> {
-        await mock.delay(10);
-
-        return { data: true };
+        return createPromise('account.remove', { address });
     }
 
     public static async renameAccount(address: string, name: string): Promise<IResult<boolean>> {
-        await mock.delay(10);
-
-        return { data: true };
+        return createPromise('account.rename', { address, name });
     }
 
     public static async getAccountList(): Promise<IResult<IAccountInfo[]>> {
-        await mock.delay(10);
+        return createPromise('account.list');
+    }
 
-        return mock.accountListResult;
+    public static async ping(): Promise<IResult<object>> {
+        return createPromise('ping', {ping: true});
     }
 
     public static async getCurrencyList(): Promise<IResult<ICurrencyInfo[]>> {
-        await mock.delay(10);
-
-        return mock.currencyListResult;
+        return createPromise('account.getCurrencies');
     }
 
     public static async send(tx: ISendTransaction, password: string)
     : Promise<IResult<ISendTransactionResult>> {
-        await mock.delay(10);
-
-        return mock.send(tx, password);
+        return createPromise('account.send', {...tx, password });
     }
 
     public static async getSendTransactionList(
-        filters?: {
-            currencyAddress?: string,
-            toAddress?: string,
-            fromAddress?: string,
-            timeStart?: number,
-            timeEnd?: number,
-        },
+        filters?: ITxListFilter,
         limit?: number,
         offset?: number,
     ): Promise<IResult<[ISendTransactionResult[], number]>> {
-        await mock.delay(1000);
-
-        return mock.transactionListResult;
+        return createPromise('transaction.list', { filters, limit, offset });
     }
 
     public static async getGasPrice(): Promise<IResult<string>> {
-        await mock.delay(10);
-
-        return { data: '10000' };
+        return createPromise('account.getGasPrice');
     }
 }
 
