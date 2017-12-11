6d14f70a09279b48f0b426ccaf5acb237d4d9e81
dmitrypisanko <dmitry.pisanko@gmail.com>
2017-12-11 15:32:05 +0300

    Format

--- front/config/webpack.config.js
+++ front/config/webpack.config.js
@@ -4,7 +4,7 @@ const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-const { getFullPath, readJson } = require('./utils');
+const {getFullPath, readJson} = require('./utils');
 const extractLess = new ExtractTextPlugin('./style.css');
 const ShakePlugin = require('webpack-common-shake').Plugin;
 
@@ -27,8 +27,8 @@ module.exports = {
     },
 
     resolve: {
-        modules: [ 'node_modules' ],
-        extensions: [ '.js', '.json', '.jsx', '.ts', '.tsx' ],
+        modules: ['node_modules'],
+        extensions: ['.js', '.json', '.jsx', '.ts', '.tsx'],
         alias: {
             './guide.less$': getFullPath('src/app/less/guide.less'),
             'app': getFullPath('src/app'),
@@ -63,12 +63,12 @@ module.exports = {
                     test: /\.less$/,
                     use: extractLess.extract({
                         fallback: 'style-loader',
-                        use: [ 'css-loader', 'less-loader' ],
+                        use: ['css-loader', 'less-loader'],
                     }),
                 },
                 {
                     test: /\.worker\.ts$/,
-                    use: [ {
+                    use: [{
                         loader: 'worker-loader',
                         options: {
                             name: '[name].js',
@@ -76,7 +76,7 @@ module.exports = {
                         },
                     }, {
                         loader: 'ts-loader',
-                    } ],
+                    }],
                 },
             ];
 
@@ -89,7 +89,7 @@ module.exports = {
 
     // devtool: isDev ? 'source-map' : false,
 
-	plugins: (() => {
+    plugins: (() => {
         const plugins = [
             process.env.WEBPACK_ANALYZE
                 ? new BundleAnalyzerPlugin()
@@ -126,26 +126,26 @@ module.exports = {
             //   ? new webpack.HotModuleReplacementPlugin()
             //   : null,
 
-	        isDev ? null : new UglifyJsPlugin({
-		        uglifyOptions: {
-			        output: {
-				        comments: false,
-				        beautify: false,
-				        ascii_only: true,
-			        },
-		        }
+            isDev ? null : new UglifyJsPlugin({
+                uglifyOptions: {
+                    output: {
+                        comments: false,
+                        beautify: false,
+                        ascii_only: true,
+                    },
+                }
             }),
 
-            new webpack.EnvironmentPlugin([ 'NODE_ENV' ]),
+            new webpack.EnvironmentPlugin(['NODE_ENV']),
 
-	        new HtmlWebpackPlugin({
-		        inject: true,
-		        template: getFullPath('./assets/entry.html'),
-	        }),
+            new HtmlWebpackPlugin({
+                inject: true,
+                template: getFullPath('./assets/entry.html'),
+            }),
 
-	        extractLess,
+            extractLess,
 
-	        isDev ? null : new StyleExtHtmlWebpackPlugin(),
+            isDev ? null : new StyleExtHtmlWebpackPlugin(),
         ];
 
         return plugins.filter(x => x);
