fe124121792fb2115db08f8f361d2bf96e84a8f5
sergey_utkin <sergey_utkin@sonm.io>
2017-12-19 18:16:43 +0300

    fix

--- front/src/app/api/error-messages.ts
+++ front/src/app/api/error-messages.ts
@@ -3,7 +3,7 @@ import { IValidation } from 'ipc/types';
 export const messages: IValidation = {
     path_not_valid: 'Incorrect file path',
     password_not_valid: 'Password is not valid',
-    gas_to_low: 'Intrinsic gas too low',
+    gas_too_low: 'Intrinsic gas too low',
 };
 
 export function getMessageText(code: string) {
