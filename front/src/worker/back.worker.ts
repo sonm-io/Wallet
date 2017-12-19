0eb2e1e3dba69e3d021c098b953f8c5bf27020da
dmitrypisanko <dmitry.pisanko@gmail.com>
2017-12-19 17:59:24 +0300

    gas_too_low error

--- front/src/worker/back.worker.ts
+++ front/src/worker/back.worker.ts
@@ -15,6 +15,8 @@ ipc.on(async (request: Request) => {
 
         if (err.message.includes('Invalid JSON RPC response from provider')) {
             err.message = 'network_error';
+        } else if (err.message.includes('Error: intrinsic gas too low')) {
+            err.message = 'gas_too_low';
         }
 
         response = new Response('api', request.requestId, null, null, err.message);
