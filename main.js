846c86c807cae0fa8e51d3947cccaf02c6850e41
dmitrypisanko <dmitry.pisanko@gmail.com>
2018-02-01 14:04:05 +0300

    WAL-22
    Fix electron window only by width, add copy/paste, toggle dev-tools

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ const url = require('url');
 const electron = require('electron');
 const BrowserWindow = electron.BrowserWindow;
 const app = electron.app;
+const Menu = electron.Menu;
+const MenuItem = electron.MenuItem;
 
 let mainWindow = null;
 
@@ -26,10 +28,13 @@ function initialize () {
 
 function createWindow () {
     const windowOptions = {
+        title: app.getName(),
+        //resizable: true,
         width: 1280,
         height: 840,
-        title: app.getName(),
-        resizable: false,
+        minWidth: 1280,
+        minHeight: 840,
+        maxWidth: 1280,
         webPreferences: {
             webSecurity: false
         }
@@ -49,6 +54,131 @@ function createWindow () {
     mainWindow.on('closed', function () {
         mainWindow = null;
     });
+
+    // Create the Application's main menu
+    var template = [{
+        label: 'Electron',
+        submenu: [
+            {
+                label: 'About Electron',
+                selector: 'orderFrontStandardAboutPanel:'
+            },
+            {
+                type: 'separator'
+            },
+            {
+                label: 'Services',
+                submenu: []
+            },
+            {
+                type: 'separator'
+            },
+            {
+                label: 'Hide Electron',
+                accelerator: 'Command+H',
+                selector: 'hide:'
+            },
+            {
+                label: 'Hide Others',
+                accelerator: 'Command+Shift+H',
+                selector: 'hideOtherApplications:'
+            },
+            {
+                label: 'Show All',
+                selector: 'unhideAllApplications:'
+            },
+            {
+                type: 'separator'
+            },
+            {
+                label: 'Quit',
+                accelerator: 'Command+Q',
+                click: function() { app.quit(); }
+            },
+        ]
+    },
+        {
+            label: 'Edit',
+            submenu: [
+                {
+                    label: 'Undo',
+                    accelerator: 'Command+Z',
+                    selector: 'undo:'
+                },
+                {
+                    label: 'Redo',
+                    accelerator: 'Shift+Command+Z',
+                    selector: 'redo:'
+                },
+                {
+                    type: 'separator'
+                },
+                {
+                    label: 'Cut',
+                    accelerator: 'Command+X',
+                    selector: 'cut:'
+                },
+                {
+                    label: 'Copy',
+                    accelerator: 'Command+C',
+                    selector: 'copy:'
+                },
+                {
+                    label: 'Paste',
+                    accelerator: 'Command+V',
+                    selector: 'paste:'
+                },
+                {
+                    label: 'Select All',
+                    accelerator: 'Command+A',
+                    selector: 'selectAll:'
+                },
+            ]
+        },
+        {
+            label: 'View',
+            submenu: [
+                {
+                    label: 'Reload',
+                    accelerator: 'Command+R',
+                    click: function() { BrowserWindow.getFocusedWindow().reloadIgnoringCache(); }
+                },
+                {
+                    label: 'Toggle DevTools',
+                    accelerator: 'Alt+Command+I',
+                    click: function() { BrowserWindow.getFocusedWindow().toggleDevTools(); }
+                },
+            ]
+        },
+        {
+            label: 'Window',
+            submenu: [
+                {
+                    label: 'Minimize',
+                    accelerator: 'Command+M',
+                    selector: 'performMiniaturize:'
+                },
+                {
+                    label: 'Close',
+                    accelerator: 'Command+W',
+                    selector: 'performClose:'
+                },
+                {
+                    type: 'separator'
+                },
+                {
+                    label: 'Bring All to Front',
+                    selector: 'arrangeInFront:'
+                },
+            ]
+        },
+        {
+            label: 'Help',
+            submenu: []
+        }];
+
+    menu = Menu.buildFromTemplate(template);
+    Menu.setApplicationMenu(menu);
 }
 
 initialize();
