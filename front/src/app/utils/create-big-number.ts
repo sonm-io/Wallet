8ab9dee25725358b91e399160c762d1f077d5c75
sergey_utkin <sergey_utkin@sonm.io>
2018-02-13 14:29:36 +0300

    WAL-51 bugfixes

--- front/src/app/utils/create-big-number.ts
+++ front/src/app/utils/create-big-number.ts
@@ -1,9 +1,12 @@
 import * as BigNumber from 'bignumber.js';
 
-export function createBigNumber(value: string, dp: number = 18) {
+export function createBigNumber(value: string, dp?: number) {
     try {
         const bn  = new BigNumber(value);
-        return new BigNumber(bn.toFixed(dp));
+
+        return dp
+            ? new BigNumber(bn.toFixed(dp))
+            : bn;
     } catch (e) {
         return undefined;
     }
