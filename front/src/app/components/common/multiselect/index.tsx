8f767a0a249c57590d98e30e07dcd85d20708cc6
sergey_utkin <sergey_utkin@sonm.io>
2018-05-03 15:15:33 +0300

    multiselect improves

--- front/src/app/components/common/multiselect/index.tsx
+++ front/src/app/components/common/multiselect/index.tsx
@@ -1,50 +1,40 @@
 import * as React from 'react';
 import * as get from 'lodash/fp/get';
 import {
-    IMultiSelectInputProps,
-    IMultiSelectOptionalInputProps,
+    IMultiSelectProps,
+    IMultiSelectAllProps,
+    IMultiSelectOptionalProps,
 } from './types';
-//import { Input } from '../input';
 import { Button } from '../button';
 import { DropdownInput } from '../dropdown-input';
 import { Checkbox } from '../checkbox';
 import { ITogglerChangeParams } from '../toggler';
-import * as debounce from 'lodash/fp/debounce';
 import Icon from '../icon';
+import * as cn from 'classnames';
 
-const DebounceWait = 100; //ms
-
-export class MultiSelect<T> extends React.Component<
-    IMultiSelectInputProps<T>,
-    any
-> {
-    public static readonly defaultProps: IMultiSelectOptionalInputProps = {
+export class MultiSelect<T> extends React.Component<IMultiSelectProps<T>, any> {
+    public static readonly defaultProps: IMultiSelectOptionalProps = {
         label: '',
         className: '',
         nameIndex: '',
-        valueIndex: '',
         filterPlaceHolder: '',
-        panelStyle: {},
     };
 
-    public constructor(props: IMultiSelectInputProps<T>) {
+    public constructor(props: IMultiSelectProps<T>) {
         super(props);
 
         this.state = {
             filteredList: props.list,
+            isExpanded: false,
         };
-
-        this.renderFilteredList = this.renderFilteredList.bind(this);
     }
 
     // Helpers
 
-    protected getValue = (item: any) => {
-        return this.props.valueIndex ? get(this.props.valueIndex)(item) : item;
-    };
-
-    protected getName = (item: any) => {
-        return this.props.nameIndex ? get(this.props.nameIndex)(item) : item;
+    protected getName = (item: T): string => {
+        return this.props.nameIndex
+            ? get(this.props.nameIndex)(item)
+            : String(item);
     };
 
     protected getLabelValue = () => {
@@ -53,82 +43,57 @@ export class MultiSelect<T> extends React.Component<
             : this.props.value.length.toString();
     };
 
-    protected filterList = debounce(DebounceWait)((value: string) => {
+    protected filterList = (value: string): T[] => {
         const filteredInputValue = value.trim().toLowerCase();
-        const filteredList =
-            filteredInputValue === ''
-                ? this.props.list
-                : this.props.list.filter(item =>
-                      this.getName(item)
-                          .toLowerCase()
-                          .includes(filteredInputValue),
-                  );
-
-        this.setState({
-            filteredList,
-        });
-    });
-
-    protected createListItem(item: any) {
-        const value = this.getValue(item);
-        const name = this.getName(item);
-        const checked = this.props.value.some(
-            row => this.getValue(row) === value,
-        );
-
-        return (
-            <div key={value}>
-                <Checkbox
-                    name={value}
-                    title={name}
-                    value={checked}
-                    onChange={this.onChangeCheckbox}
-                />
-            </div>
-        );
-    }
-
-    protected renderFilteredList() {
-        return this.state.filteredList.map((i: T) =>
-            this.createListItem.call(this, i),
-        );
-    }
+        return filteredInputValue === ''
+            ? this.props.list
+            : this.props.list.filter(item =>
+                  this.getName(item)
+                      .toLowerCase()
+                      .includes(filteredInputValue),
+              );
+    };
 
     // Event Handlers
 
-    protected onChangeFilter = (event: any) => {
+    protected handleChangeFilter = (event: any) => {
         const value = event.target.value;
-        this.filterList(value);
+        this.setState({
+            filteredList: this.filterList(value),
+        });
     };
 
-    protected onRequireClose = () => {
-        this.filterList('');
+    protected handleRequireClose = () => {
+        this.setState({
+            filteredList: this.props.list,
+            isExpanded: false,
+        });
+
         this.props.onRequireClose && this.props.onRequireClose();
     };
 
-    protected onChangeCheckbox = (params: ITogglerChangeParams) => {
-        let itemValue = params.name;
-        let value = this.props.value.slice(0);
-        if (params.value) {
-            value.push(this.state.filteredList.find(
-                (row: T) => this.getValue(row) === itemValue,
-            ) as T);
-        } else {
-            value = value.filter(item => this.getValue(item) !== itemValue);
-        }
+    protected handleChangeCheckbox = (params: ITogglerChangeParams) => {
+        const itemName = params.name;
+
+        const value: T[] = params.value
+            ? [
+                  ...this.props.value,
+                  this.props.list.find(x => this.getName(x) === itemName) as T,
+              ]
+            : this.props.value.filter(x => this.getName(x) !== itemName);
 
         this.props.onChange({
             value,
             name: this.props.name,
-            valueString: params.name,
+            valueString: value.map(this.getName).join(', '),
         });
     };
 
-    protected onClickApply = () => {
-        this.props.onRequireClose && this.props.onRequireClose();
-    };
+    protected handleClickClear = () => {
+        this.setState({
+            isExpanded: false,
+        });
 
-    protected onClickClear = () => {
         this.props.onChange({
             value: [],
             name: this.props.name,
@@ -136,51 +101,68 @@ export class MultiSelect<T> extends React.Component<
         });
     };
 
-    public render() {
-        const { className } = this.props;
+    protected handleButtonClick = () => {
+        this.setState({
+            isExpanded: !this.state.isExpanded,
+        });
+    };
 
-        const panelStyle =
-            this.props.panelStyle || MultiSelect.defaultProps.panelStyle;
+    public render() {
+        const p = this.props as IMultiSelectAllProps<T>;
+        const valueNames = p.value.map(this.getName);
 
         return (
-            <div className="multiselect__container">
+            <div className={cn('multiselect__container', p.className)}>
                 <DropdownInput
-                    className={className}
-                    valueString={`${this.props.label}: ${this.getLabelValue()}`}
-                    onRequireClose={this.onRequireClose}
-                    onButtonClick={this.props.onButtonClick}
+                    className="multiselect__dropdown-input"
+                    valueString={`${p.label}: ${this.getLabelValue()}`}
+                    onRequireClose={this.handleRequireClose}
+                    onButtonClick={this.handleButtonClick}
                     hasBalloon
-                    isExpanded={this.props.isExpanded}
+                    isExpanded={this.state.isExpanded}
                 >
-                    <div style={panelStyle}>
+                    <div className="multiselect__popup">
                         <div className="multiselect__input-container">
                             <input
                                 className="multiselect__input"
                                 name="filter"
-                                onChange={this.onChangeFilter}
-                                placeholder={this.props.filterPlaceHolder}
+                                onChange={this.handleChangeFilter}
+                                placeholder={p.filterPlaceHolder}
                             />
                         </div>
 
                         <div className="multiselect__list-container">
-                            {this.renderFilteredList()}
+                            {this.state.filteredList.map((i: T) => {
+                                const name = this.getName(i);
+
+                                return (
+                                    <Checkbox
+                                        className="multiselect__item"
+                                        key={name}
+                                        name={name}
+                                        title={name}
+                                        value={valueNames.indexOf(name) !== -1}
+                                        onChange={this.handleChangeCheckbox}
+                                    />
+                                );
+                            })}
                         </div>
 
                         <div className="multiselect__apply-button-container">
                             <Button
                                 color="violet"
                                 className="multiselect__apply-button"
-                                onClick={this.onClickApply}
+                                onClick={this.handleRequireClose}
                             >
                                 APPLY
                             </Button>
                         </div>
                     </div>
                 </DropdownInput>
-                {this.props.clearBtn ? (
+                {p.hasClearButton ? (
                     <button
                         className="multiselect__clear-button"
-                        onClick={this.onClickClear}
+                        onClick={this.handleClickClear}
                     >
                         <Icon i="Close" />
                     </button>
