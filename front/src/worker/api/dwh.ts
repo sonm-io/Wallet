71dca978ad36cf86ffdeecb45a3f870aafc25f67
sergey_utkin <sergey_utkin@sonm.io>
2018-05-08 18:22:22 +0300

    WIP

--- front/src/worker/api/dwh.ts
+++ front/src/worker/api/dwh.ts
@@ -24,7 +24,7 @@ export class DWH {
         this.url = url;
     }
 
-    public getProfiles = async (): Promise<t.IProfileListResult> => {
+    public getProfiles = async (): Promise<t.IListResult<t.IProfileBrief>> => {
         const res = await this.fetchData('GetProfiles');
         const records = [] as t.IProfileBrief[];
 
