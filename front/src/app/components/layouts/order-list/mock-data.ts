add5e399e940d7365b9f091b08929031885098bf
maestrow <andrew.shmelev@gmail.com>
2018-05-14 17:19:42 +0300

    OrderListView: recover mock data to view component in sg.

---
+++ front/src/app/components/layouts/order-list/mock-data.ts
@@ -0,0 +1,49 @@
+import { IOrder } from 'app/api/types';
+
+// Taken from ReactDevTool
+const data: Array<IOrder> = [
+    {
+        id: '3',
+        orderType: 1,
+        creatorStatus: 0,
+        creatorName: '',
+        price: '41999999999999997600',
+        duration: 4,
+        orderStatus: 2,
+        authorID: '0x8125721C2413d99a33E351e1F6Bb4e56b6b633FD',
+        cpuCount: 1,
+        gpuCount: 1,
+        hashrate: 0,
+        ramSize: 954,
+    },
+    {
+        id: '9',
+        orderType: 1,
+        creatorStatus: 0,
+        creatorName: '',
+        price: '2499999999999998400',
+        duration: 8,
+        orderStatus: 2,
+        authorID: '0x8125721C2413d99a33E351e1F6Bb4e56b6b633FD',
+        cpuCount: 1,
+        gpuCount: 0,
+        hashrate: 0,
+        ramSize: 1,
+    },
+    {
+        id: '10',
+        orderType: 1,
+        creatorStatus: 0,
+        creatorName: '',
+        price: '999999999999997200',
+        duration: 4,
+        orderStatus: 2,
+        authorID: '0x8125721C2413d99a33E351e1F6Bb4e56b6b633FD',
+        cpuCount: 1,
+        gpuCount: 0,
+        hashrate: 0,
+        ramSize: 1,
+    },
+];
+
+export { data };
