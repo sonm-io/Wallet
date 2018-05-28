2eecdbac81e273e406bc3d75dc0b665277a05ab4
maestrow <andrew.shmelev@gmail.com>
2018-05-28 12:23:30 +0300

    Password component with eye

--- front/src/app/components/common/password/index.tsx
+++ front/src/app/components/common/password/index.tsx
@@ -1,28 +1,39 @@
 import * as React from 'react';
 import { Input, IPasswordInputProps } from 'app/components/common/input';
 import { Icon } from 'app/components/common/icon';
-import * as cn from 'classnames';
 
-interface IState {}
+interface IState {
+    showPassword: boolean;
+}
 
 export class Password extends React.Component<IPasswordInputProps, IState> {
+    constructor(props: IPasswordInputProps) {
+        super(props);
+        this.state = { showPassword: false };
+    }
+
+    protected handleClickEye = () => {
+        this.setState({ showPassword: !this.state.showPassword });
+    };
+
+    protected renderEye = () => {
+        return (
+            <button
+                className="password__eye-button"
+                onClick={this.handleClickEye}
+            >
+                <Icon i={this.state.showPassword ? 'EyeOff' : 'Eye'} />
+            </button>
+        );
+    };
+
     public render() {
         return (
-            <div
-                className={cn('password', this.props.className, {
-                    'password--readonly': this.props.readOnly,
-                })}
-            >
-                <Input
-                    {...this.props}
-                    className="password__input"
-                    type="password"
-                />
-                <div className="password__icon-container">
-                    <Icon i="Exit" />
-                    <div className="password__underline" />
-                </div>
-            </div>
+            <Input
+                {...this.props}
+                type={this.state.showPassword ? 'text' : 'password'}
+                postfix={this.renderEye()}
+            />
         );
     }
 }
