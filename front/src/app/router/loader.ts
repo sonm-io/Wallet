b15b76f42dfac1db65ca972e20ebe79233b94dfc
sergey_utkin <sergey_utkin@sonm.io>
2018-07-10 16:01:37 +0300

    fixed setting up filter from buyed order

--- front/src/app/router/loader.ts
+++ front/src/app/router/loader.ts
@@ -2,8 +2,12 @@ import { rootStore } from 'app/stores';
 import { IOrderFilter } from 'app/stores/order-filter';
 import { IOrder } from 'app/api/types';
 import { TEthereumAddress } from '../entities/types';
+import moveDecimalPoint from '../utils/move-decimal-point';
+import { BN } from 'app/utils/create-big-number';
 
 const str = (x: any) => (x === undefined ? '' : String(x));
+
+const SECS_IN_HOUR = new BN('3600');
 
 export class DataLoader {
     public loadOrder(orderId: string) {
@@ -17,14 +21,17 @@ export class DataLoader {
     public loadOrderListByOrder(order: IOrder) {
         this.loadOrderListByParams({
             side: 'Sell',
-            priceFrom: str(order.usdWeiPerSeconds),
+            priceFrom: moveDecimalPoint(
+                new BN(order.usdWeiPerSeconds).mul(SECS_IN_HOUR).toString(),
+                -18,
+                4,
+            ),
             redshiftFrom: str(order.benchmarkMap.redshiftGpu),
             ethFrom: str(order.benchmarkMap.ethHashrate),
             zCashFrom: str(order.benchmarkMap.zcashHashrate),
             cpuCountFrom: str(order.benchmarkMap.cpuCount),
             gpuCountFrom: str(order.benchmarkMap.gpuCount),
             ramSizeFrom: str(order.benchmarkMap.ramSize),
-            storageSizeFrom: str(order.benchmarkMap.storageSize),
             gpuRamSizeFrom: str(order.benchmarkMap.gpuRamSize),
         });
     }
