065d2e4c08ceb6da4960964d37e5c67e51c57195
maestrow <andrew.shmelev@gmail.com>
2018-08-15 17:12:58 +0300

    GUI-258. simplify setIntervalAsync

--- front/src/app/utils/set-interval-async.ts
+++ front/src/app/utils/set-interval-async.ts
@@ -1,10 +1,7 @@
-import { delay } from './async-delay';
-
 export const setIntervalAsync = async (
     asyncFn: () => Promise<void>,
     interval: number,
 ) => {
     await asyncFn();
-    await delay(interval);
-    setTimeout(() => setIntervalAsync(asyncFn, interval), 0);
+    setTimeout(() => setIntervalAsync(asyncFn, interval), interval);
 };
