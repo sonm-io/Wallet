f6111f9bf4bc4bef92dfc3faec042a68d8484f6c
maestrow <andrew.shmelev@gmail.com>
2018-08-16 21:36:26 +0300

    GUI-258. fix sg: remove unused import

--- front/src/worker/back.worker.ts
+++ front/src/worker/back.worker.ts
@@ -1,29 +1,28 @@
 import ipc from './ipc';
 import { api } from './api';
 import { TResultPromise, IValidation } from '../ipc/types';
-import { instanceOf } from 'prop-types';
 
-ipc.setRequestProcessor(async (type: string, payload: any): TResultPromise<
-    any
-> => {
-    try {
-        const { data, validation } = await api.resolve(type, payload);
+ipc.setRequestProcessor(
+    async (type: string, payload: any): TResultPromise<any> => {
+        try {
+            const { data, validation } = await api.resolve(type, payload);
 
-        return {
-            data,
-            validation: validation as IValidation,
-        };
-    } catch (err) {
-        if (IS_DEV) {
-            console.error(type, payload);
-            console.error(err);
+            return {
+                data,
+                validation: validation as IValidation,
+            };
+        } catch (err) {
+            if (IS_DEV) {
+                console.error(type, payload);
+                console.error(err);
+            }
+
+            throw Error(
+                (err instanceof Error ? err.message : String(err)).replace(
+                    /^Error: /,
+                    '',
+                ),
+            );
         }
-
-        throw Error(
-            (err instanceof Error ? err.message : String(err)).replace(
-                /^Error: /,
-                '',
-            ),
-        );
-    }
-});
+    },
+);
