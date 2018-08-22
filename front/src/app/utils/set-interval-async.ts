e2e4733c9b518f36550913a15deb986436e01c5a
maestrow <andrew.shmelev@gmail.com>
2018-08-22 19:25:43 +0300

    GUI-258. Added ability to stop a setIntervalAsync task.

--- front/src/app/utils/set-interval-async.ts
+++ front/src/app/utils/set-interval-async.ts
@@ -1,7 +1,33 @@
-export const setIntervalAsync = async (
-    asyncFn: () => Promise<void>,
+class IntervalTask {
+    protected activeValue = true;
+
+    public get isActive() {
+        return this.activeValue;
+    }
+
+    public stop = () => {
+        this.activeValue = false;
+    };
+}
+
+const call = async (
+    asyncFn: (...args: any[]) => Promise<void>,
     interval: number,
+    task: IntervalTask,
+    args: any[],
 ) => {
-    await asyncFn();
-    setTimeout(() => setIntervalAsync(asyncFn, interval), interval);
+    if (task.isActive) {
+        await asyncFn(...args);
+        setTimeout(() => call(asyncFn, interval, task, args), interval);
+    }
+};
+
+export const setIntervalAsync = (
+    asyncFn: (...args: any[]) => Promise<void>,
+    interval: number,
+    ...args: any[]
+) => {
+    const task = new IntervalTask();
+    call(asyncFn, interval, task, args);
+    return task;
 };
